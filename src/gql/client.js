8987518c846d06909f4ec27838e12cc41fb916fd
Rachmizard <rachmizard11072000@gmail.com>
2021-08-24 12:40:50 +0700

    add: apollo server express package

--- src/gql/client.js
+++ src/gql/client.js
@@ -1,17 +1,18 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
 import { resolvers, typeDefs } from "./registerSchema";
 
 export async function startApolloServer() {
+	const app = express();
 	const server = new ApolloServer({
 		typeDefs,
 		resolvers,
-		context: ({ req }) => {
-			const token = req.get("Authorization") || "";
-			return {
-				token,
-			};
-		},
 	});
-	const { url } = await server.listen();
-	console.log(`🚀 Server ready at ${url}`);
+	await server.start();
+	server.applyMiddleware({ app });
+	app.listen({ port: 4000 }, () => {
+		console.log(
+			`🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+		);
+	});
 }
